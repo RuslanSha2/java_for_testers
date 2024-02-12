41ae406ee9b4e8b91d8acee1131dc36681e19806
Шарипов Руслан Фаилович <Sharipov.RF@gazprom-neft.local>
2024-02-12 12:12:53 +0300

    to#9.2

--- mantis_tests/src/test/java/ru/ctqa/mantis/tests/UserRegistrationsTests.java
+++ mantis_tests/src/test/java/ru/ctqa/mantis/tests/UserRegistrationsTests.java
@@ -6,19 +6,17 @@ import org.junit.jupiter.params.provider.MethodSource;
 import ru.ctqa.mantis.common.CommonFunctions;
 
 import java.time.Duration;
-import java.util.function.Supplier;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 public class UserRegistrationsTests extends TestBase {
 
-    public static Stream<String> randomUsers() {
-        Supplier<String> randomUser = () -> CommonFunctions.randomString(8);
-        return Stream.generate(randomUser).limit(1);
+    public static Stream<String> randomUser() {
+        return Stream.of(CommonFunctions.randomString(8));
     }
 
     @ParameterizedTest
-    @MethodSource("randomUsers")
+    @MethodSource("randomUser")
     void canRegisterUser(String username) throws InterruptedException {
         // создать пользователя (адрес) на почтовом сервисе (JamesHelper)
         var email = String.format("%s@localhost", username);
@@ -26,7 +24,9 @@ public class UserRegistrationsTests extends TestBase {
         app.jamesCli().addUser(email, password);
 
         // заполняем форму создания и отправляем (SessionHelper)
-        app.session().login("administrator", "root");
+        app.session().login(
+                app.property("web.username"),
+                app.property("web.password"));
         app.session().createUser(username, email);
         Assertions.assertTrue(app.session().existUser(username, email));
 
@@ -47,6 +47,36 @@ public class UserRegistrationsTests extends TestBase {
         Assertions.assertTrue(app.session().isLoginForm());
 
         // проверяем, что пользователь может залогиниться (HttpSessionHelper)
+        app.http().login(username, password);
+        Assertions.assertTrue(app.http().isLoggedIn());
+    }
+
+    @ParameterizedTest
+    @MethodSource("randomUser")
+    void canCreateUser(String username) throws InterruptedException {
+        var email = String.format("%s@localhost", username);
+        var password = "root";
+        app.jamesApi().addUser(email, password);
+
+        app.session().login(
+                app.property("web.username"),
+                app.property("web.password"));
+        app.session().createUser(username, email);
+        Assertions.assertTrue(app.session().existUser(username, email));
+
+        var duration = Duration.ofSeconds(60);
+        var messages = app.mail().receive(email, password, duration);
+        Assertions.assertEquals(1, messages.size());
+
+        var text = messages.get(0).content();
+        var pattern = Pattern.compile("http://\\S*");
+        var matcher = pattern.matcher(text);
+        Assertions.assertTrue(matcher.find());
+        var url = text.substring(matcher.start(), matcher.end());
+
+        app.session().confirmPassword(url, password);
+        Assertions.assertTrue(app.session().isLoginForm());
+
         app.http().login(username, password);
         Assertions.assertTrue(app.http().isLoggedIn());
     }
