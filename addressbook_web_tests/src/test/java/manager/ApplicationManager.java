4b94b53696b334c5d12ea118eab75fd03a161a52
Шарипов Руслан Фаилович <Sharipov.RF@gazprom-neft.local>
2024-02-21 11:09:07 +0300

    Lesson#10.5

--- addressbook_web_tests/src/test/java/manager/ApplicationManager.java
+++ addressbook_web_tests/src/test/java/manager/ApplicationManager.java
@@ -5,11 +5,16 @@ import org.openqa.selenium.Dimension;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.edge.EdgeDriverService;
 import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+
+import java.net.URL;
 import java.util.Properties;
 
 public class ApplicationManager {
@@ -22,18 +27,31 @@ public class ApplicationManager {
 
     private Properties properties;
 
-    public void init(String browser, Properties properties) {
+    public void init(String browser, Properties properties) throws MalformedURLException {
         this.properties = properties;
 
         if (driver == null) {
+            var seleniumServer = properties.getProperty("seleniumServer");
             if ("firefox".equals(browser)) {
-                driver = new FirefoxDriver();
+                if (seleniumServer != null) {
+                    driver = new RemoteWebDriver(new URL(seleniumServer), new FirefoxOptions());
+                } else {
+                    driver = new FirefoxDriver();
+                }
             } else if ("chrome".equals(browser)) {
-                driver = new ChromeDriver();
+                if (seleniumServer != null) {
+                    driver = new RemoteWebDriver(new URL(seleniumServer), new ChromeOptions());
+                } else {
+                    driver = new ChromeDriver();
+                }
             } else if ("edge".equals(browser)) {
-                var edgeOpt = new EdgeOptions();
-                edgeOpt.addArguments("headless");
-                driver = new EdgeDriver(edgeOpt);
+                if (seleniumServer != null) {
+                    driver = new RemoteWebDriver(new URL(seleniumServer), new EdgeOptions());
+                } else {
+                    var edgeOpt = new EdgeOptions();
+                    edgeOpt.addArguments("headless");
+                    driver = new EdgeDriver(edgeOpt);
+                }
             } else {
                 throw new IllegalArgumentException(String.format("Unknown browser %s", browser));
             }
